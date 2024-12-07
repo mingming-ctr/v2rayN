57c2121a6e420f15794aec4113ecc630fcdcc3f4
mayb <yongcheng.m@gmail.com>
2024-12-07 12:22:36 +0800

    build2.ps1 删除

--- v2rayN/build2.ps1
+++ v2rayN/build2.ps1
@@ -16,43 +16,12 @@ dotnet publish `
 	-p:PublishSingleFile=true `
 	-o "$OutputPath/win-x64"
 
-dotnet publish `
-	./v2rayN/v2rayN.csproj `
-	-c Release `
-	-r win-arm64 `
-	--self-contained false `
-	-p:PublishReadyToRun=false `
-	-p:PublishSingleFile=true `
-	-o "$OutputPath/win-arm64"
-
-dotnet publish `
-	./v2rayN.Desktop/v2rayN.Desktop.csproj `
-	-c Release `
-	-r linux-x64 `
-	--self-contained true `
-	-p:PublishReadyToRun=false `
-	-p:PublishSingleFile=true `
-	-o "$OutputPath/linux-x64"
-	
-dotnet publish `
-	./v2rayN.Desktop/v2rayN.Desktop.csproj `
-	-c Release `
-	-r linux-arm64 `
-	--self-contained true `
-	-p:PublishReadyToRun=false `
-	-p:PublishSingleFile=true `
-	-o "$OutputPath/linux-arm64"
- 
-
 if ( -Not $? ) {
 	exit $lastExitCode
 	}
 
 if ( Test-Path -Path ./bin/v2rayN ) {
     rm -Force "$OutputPath/win-x64/*.pdb"
-    rm -Force "$OutputPath/win-arm64/*.pdb"
-    rm -Force "$OutputPath/linux-x64/*.pdb"
-    rm -Force "$OutputPath/linux-arm64/*.pdb"
 }
 
 Write-Host 'Build done'
